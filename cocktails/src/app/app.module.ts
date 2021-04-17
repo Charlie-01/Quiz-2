28052b0720d84540c7186cba4f6f80ba6fb16be4
Charlie-01 <55993688+Charlie-01@users.noreply.github.com>
2021-04-17 16:34:53 -0400

    i rly hope the node modules dont push, but this is the setup of the site

---
+++ cocktails/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeComponent,
+    FormComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
